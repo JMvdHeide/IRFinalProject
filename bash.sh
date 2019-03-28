2255b0c03a8a9107711f49c9a02cc50ec07fe7d8
Nick <nick_dude@live.nl>
2019-03-28 20:41:57 +0100

    Script to split data into training, test and development set

---
+++ bash.sh
@@ -0,0 +1,26 @@
+#! /bin/bash 
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/01/20180115\:09.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Ochtend/15012018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/02/20180215\:09.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Ochtend/15022018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/03/20180315\:09.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Ochtend/15032018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/04/20180415\:09.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Ochtend/15042018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/05/20180515\:09.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Ochtend/15052018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/06/20180615\:09.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Ochtend/15062018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/07/20180715\:09.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Ochtend/15072018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/08/20180815\:09.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Ochtend/15082018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/09/20180915\:09.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Ochtend/15092018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/10/20181015\:09.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Ochtend/15102018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/11/20181115\:09.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Ochtend/15112018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/12/20181215\:09.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Ochtend/15122018.txt
+
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/01/20180115\:18.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Middag/15012018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/02/20180215\:18.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Middag/15022018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/03/20180315\:18.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Middag/15032018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/04/20180415\:18.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Middag/15042018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/05/20180515\:18.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Middag/15052018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/06/20180615\:19.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Middag/15062018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/07/20180715\:18.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Middag/15072018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/08/20180815\:18.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Middag/15082018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/09/20180915\:18.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Middag/15092018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/10/20181015\:18.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Middag/15102018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/11/20181115\:18.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Middag/15112018.txt
+gunzip -c /net/corpora/twitter2_en/Tweets/2016/12/20181215\:18.out.gz | /net/corpora/twitter2/tools/tweet2tab -i user text > /home/s3094731/IR-project/Middag/15122018.txt
---
+++ split_data.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+# The file cutter 3000
+# This script will take an argument that needs to be a file
+# it will randomise this file so everything is in another place
+# After that it will cut 80% and write it to a train file and the same with 10% for a test file and development file
+
+
+import sys, random
+
+
+def read_file(file):
+    """Read in the file, split each line and get only the text."""
+    print('##### Reading data...')
+    with open(file, "r") as f:
+        data = list()
+        for line in f.readlines():
+            line = line.split('\t')
+            data.append(line[1])
+    f.close()
+    return data
+
+def write_to_file(training, test, development):
+    """Write training, test and development data to all correct files."""
+    print('##### Writing to files training, test and development...')
+    with open('training_data.txt', 'w')as f:
+        for line in training:
+            f.write(line)
+    f.close()
+    with open('test_data.txt', 'w')as f:
+        for line in test:
+            f.write(line)
+    f.close()
+    with open('development_data.txt', 'w')as f:
+        for line in development:
+            f.write(line)
+    f.close()
+
+def split_and_write_files(data):
+    """Shuffle the data and split them into training, test and development set."""
+    print('##### Spliting data...')
+    random.shuffle(data)
+    len_data = len(data)
+    len_training_data = int(len_data * 0.8)
+    len_test_data = int(len_data * 0.1)
+    training_data = data[:len_training_data]
+    test_data = data[len_training_data:len_training_data + len_test_data]
+    development_data = data[len_training_data + len_test_data: len_training_data + len_test_data + len_test_data]
+    write_to_file(training_data, test_data, development_data)
+
+
+def main():
+    if len(sys.argv) > 1 and len(sys.argv) < 3:
+        print('##### Welcome to the The file cutter 3000')
+        data = read_file(sys.argv[1])
+        split_and_write_files(data)
+        print('##### Done! Created training_data.txt, test_data.txt and development_data.txt')
+    else:
+        print('You have to add an argument that represents a file to be splited.')
+
+if __name__ == '__main__':
+    main()
