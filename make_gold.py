c60a1d86e6d407f7b38a53871f69e5a422d3fec3
Willeem <willem.datema@hotmail.com>
2019-03-28 19:07:19 +0100

    csv ipv txt file

--- make_gold.py
+++ make_gold.py
@@ -4,18 +4,19 @@ import sklearn
 import json
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import operator
-
-sia = SentimentIntensityAnalyzer()
+import csv
 import time
+sia = SentimentIntensityAnalyzer()
 tweets = set()
 with open('Tweets/ochtend_tweets.txt','r') as infile:
     for line in infile.readlines():
         line = line.split('\t')
         tweets.add(line[1])
 
-with open('ochtend_tweets_gold','w') as outfile:
+with open('ochtend_tweets_gold.csv',mode='w') as outfile:
+    writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for tweet in tweets:
         scores = sia.polarity_scores(tweet)
         del scores['compound']
         sorted_scores = sorted(scores.items(),key=operator.itemgetter(1),reverse=True)
-        outfile.write("{}\t{}\n".format(tweet,sorted_scores[0][0]))
+        writer.writerow((tweet,sorted_scores[0][0]))
